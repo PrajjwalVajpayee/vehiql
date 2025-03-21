8878e0233bebe5a509d0febb669e42e23c79ecd4
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-03-21 23:42:04 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
+const inter = Inter({subsets:['latin']})
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,12 +21,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className}`}
       >
+        <Header/>
+        <main className="min-h-screen">
         {children}
+        </main>
+        <footer className="bg-blue-50 py-12">
+          <div className="text-gray-600 text-center">
+            <p> Copyright@ Gappoche</p>
+          </div>
+        </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
