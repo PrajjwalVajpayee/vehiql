ce57d42b0b810e0c0db500b3bf4824f5387749d1
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-03-24 22:59:49 +0530

    create function to check if user is admin or not

--- components/header.jsx
+++ components/header.jsx
@@ -4,10 +4,15 @@ import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
 import { ArrowLeft, CarFront, Heart, Layout } from 'lucide-react'
+import { checkUser } from '@/lib/checkUser'
 
-const Header = ({isAdminPage = false}) => {
+const Header = async ({isAdminPage = false}) => {
 
-  const isAdmin = false
+  const user= await checkUser();
+
+  const isAdmin = user?.role === 'ADMIN';
+
+
   return (
     <header className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
       <nav className=',x-auto px-4 py-4 flex items-center justify-between'>
