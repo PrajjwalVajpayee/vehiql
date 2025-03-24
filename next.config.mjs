920e1d6681bd901eec42274bb4e8028294153ea7
PrajjwalVajpayee <122729018+PrajjwalVajpayee@users.noreply.github.com>
2025-03-24 22:54:56 +0530

    added configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental={
+        serverComponentsHmrCache:false,
+    },
+   
+    images:{
+     remotePatterns:[
+        {
+            protocol:"https",
+            hostname:"fqmlhqznskkaulivqeqg.supabase.co"
+
+
+        }
+     ]
+    },
+    async headers() {
+        return [
+            {
+                source:"/embed",
+                headers:[
+                    {
+                        key:"Content-Security-Policy",
+                        value:"frame-src 'self' https://google.com"
+                    }
+                ]
+            }
+        ]
+    }
+};
 
 export default nextConfig;
